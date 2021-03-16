0b1ba6eff2e1ca694645083266e5c142e08763e3
baptistefrtr <baptistefrtr@gmail.com>
2021-03-16 16:33:18 +0100

    !live cmd add
    !live <streamerName> return all the info of a streamer if he is streaming and a OFFLINE message if not

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const env = require('dotenv').config();
+const axios = require("axios");
 
 client.on("ready", () => {
     console.log("ready!");
@@ -8,6 +9,7 @@ client.on("ready", () => {
 
 client.on("message", msg => {
     let message = msg.content
+    var string = message.split(" ")
 
     console.log(msg.author);
     if (message.startsWith("!")) {
@@ -80,6 +82,47 @@ client.on("message", msg => {
                 .setFooter('Venez jouer !', 'https://i.imgur.com/wSTFkRM.png');
 
             msg.reply(exampleEmbed);
+        }
+        if (string[0] === "!live") {
+            let config = {
+                headers: {
+                    "Authorization": "Bearer " + process.env.VUE_APP_TWITCH_ACCESS_TOKEN,
+                    "Client-ID": process.env.VUE_APP_TWITCH_API_KEY
+                }
+            }
+            axios.get("https://api.twitch.tv/helix/users?login=" + string[1], config).then((response) => {
+                if (response.status === 200 && response.data.data[0]) {
+                    //console.log(response.data.data[0].id);
+                    let id = response.data.data[0].id;
+                    if (id) {
+                        let config = {
+                            headers: {
+                                "Accept": "application/vnd.twitchtv.v5+json",
+                                "Client-ID": process.env.VUE_APP_TWITCH_API_KEY
+                            }
+                        }
+                        axios.get("https://api.twitch.tv/kraken/streams/" + id, config).then((response) => {
+                            //console.log(response.data.stream);
+                            if (response.data.stream != null) {
+                                let message = response.data.stream.channel.display_name + " is now streaming !\nThe stream is actually in the category " + response.data.stream.channel.game + " for a total of " + response.data.stream.viewers + " viewers.\nThe title of the live is '" + response.data.stream.channel.status;
+                                console.log(message);
+                                const exampleEmbed = new Discord.MessageEmbed()
+                                    .setColor('#0099ff')
+                                    .setTitle(response.data.stream.channel.display_name)
+                                    .setURL(response.data.stream.channel.url)
+                                    .setDescription(message)
+                                    .setImage(response.data.stream.channel.logo)
+                                    .setTimestamp()
+                                    .setFooter('Venez jouer !', response.data.stream.channel.logo);
+
+                                msg.reply(exampleEmbed);
+                            } else {
+                                console.log("STREAM OFFLINE");
+                            }
+                        }).catch((error) => console.log(error))
+                    }
+                }
+            }).catch((error) => console.log(error))
         }
     }
 })
