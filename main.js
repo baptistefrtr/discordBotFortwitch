00619b91db56c44b739e119f25bf20c7a160b22d
baptistefrtr <baptistefrtr@gmail.com>
2021-03-22 11:05:28 +0100

    !minecraft
    Display every informations about how to join the minecraft server

--- main.js
+++ main.js
@@ -64,6 +64,17 @@ client.on("message", msg => {
         if (message === "!clicker") {
             msg.reply("https://jam-2021.web.app/");
         }
+        if (message === "!minecraft") {
+            const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Comment rejoindre le serveur ?')
+                .setURL('https://www.twitch.tv/ccloud__')
+                .setDescription('IP : la-sainte-chemise.minesr.com\n\nVersion: 1.16.5\n\nPrenium: Oui')
+                .setTimestamp()
+                .setFooter('Venez jouer !', 'https://static-cdn.jtvnw.net/jtv_user_pictures/b4b7080d-a3af-441a-9438-6317e6bbb07b-profile_image-300x300.png');
+
+            msg.reply(exampleEmbed);
+        }
         if (message === "!bye") {
             const exampleEmbed = new Discord.MessageEmbed()
                 .setColor('#0099ff')
